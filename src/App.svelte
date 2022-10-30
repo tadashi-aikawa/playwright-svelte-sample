cfefd74ace2af49d6e7843558e9bed8a0a6ca833
tadashi-aikawa <syou.maman@gmail.com>
2022-10-30 13:34:27 +0900

    Initialize svelte project

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  let count = 0;
+</script>
+
+<main>
+  <h1>Playwright Svelte Sample</h1>
+
+  <h2>Push "Increment/Decrement" button</h2>
+
+  <div style=" justify-content: center; gap: 10px;">
+    <button on:click={() => count++}>Increment</button>
+    <button on:click={() => count--}>Decrement</button>
+  </div>
+  <div data-test-id="counter" style="font-size: 200%; padding: 30px;">
+    Count: {count}
+  </div>
+</main>
